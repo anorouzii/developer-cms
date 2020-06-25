4213e04558997bb42eb863881c1f852533b4d2f4
Ali Norouzi <anorouziiii@gmail.com>
2020-06-25 18:37:08 +0430

    fix redux devtool bug

---
+++ admin/build/precache-manifest.e4d0295535d89042710aaab3b7887d0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0389d3fe52aca568836ab7094cafe88f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "73654e89be570650b220",
+    "url": "./static/css/2.ad76a385.chunk.css"
+  },
+  {
+    "revision": "414347f8d169b09c274a",
+    "url": "./static/css/main.ddbac757.chunk.css"
+  },
+  {
+    "revision": "73654e89be570650b220",
+    "url": "./static/js/2.90d1a453.chunk.js"
+  },
+  {
+    "revision": "ca96d7d1e844bdd080f4da4174273d61",
+    "url": "./static/js/2.90d1a453.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "414347f8d169b09c274a",
+    "url": "./static/js/main.82f3f7d2.chunk.js"
+  },
+  {
+    "revision": "bf7b05610f328a6527e3",
+    "url": "./static/js/runtime-main.ccf9c1ed.js"
+  }
+]);
